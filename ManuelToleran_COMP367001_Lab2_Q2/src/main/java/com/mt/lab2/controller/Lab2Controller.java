3832b48c3df06fd5f66d3197f0ead4fc6035cdfb
Manuel Toleran <mtoleran@my.centennialcollege.ca>
2025-02-20 13:43:47 -0500

    Update name in welcome message

--- ManuelToleran_COMP367001_Lab2_Q2/src/main/java/com/mt/lab2/controller/Lab2Controller.java
+++ ManuelToleran_COMP367001_Lab2_Q2/src/main/java/com/mt/lab2/controller/Lab2Controller.java
@@ -19,13 +19,13 @@ public class Lab2Controller {
 
         // Set the message based on the time of day
         if (hour >= 5 && hour < 12) {
-            message = "Good Morning, Manuel Toleran, Welcome to COMP367";
+            message = "Good Morning, Manuel Carl, Welcome to COMP367";
         } else if (hour >= 12 && hour < 17) {
-            message = "Good Afternoon, Manuel Toleran, Welcome to COMP367";
+            message = "Good Afternoon, Manuel Carl, Welcome to COMP367";
         } else if (hour >= 17 && hour < 21) {
-            message = "Good Evening, Manuel Toleran, Welcome to COMP367";
+            message = "Good Evening, Manuel Carl, Welcome to COMP367";
         } else {
-            message = "Good Night, Manuel, Welcome to COMP367";
+            message = "Good Night, Manuel Carl, Welcome to COMP367";
         }
 
         // Add the message to the model
